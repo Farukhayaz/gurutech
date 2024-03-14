8fd8a92171a79cdf04a63c6859fef9f575bfea04
Farukh Ayaz <farukhayaz999@gmail.com>
2024-03-14 23:32:20 +0500

    nine commit

--- src/components/Cardslider.js
+++ src/components/Cardslider.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Cardslider.css'; // You can define your own CSS styles for the Cardslider
-
-// Sample image data
-const images = [
-  '/image/laptop/lap.webp',
-  '/image/laptop/13.jpeg',
-  '/image/laptop/6.jpeg',
-  '/image/laptop/10.jpeg'
-];
+import { useLocation } from 'react-router-dom';
 
 const Cardslider = () => {
+  const location = useLocation();
+  const [images, setImages] = useState([]);
+  console.log(images,"images")
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const cardData = location?.state?.cardData;
+
+  useEffect(() => {
+    const cardData = location?.state?.cardData;
+    if (cardData && cardData.img) {
+      setImages(cardData.img);
+    }
+  }, [location]);
 
   const goToPrevious = () => {
     setCurrentImageIndex((prevIndex) =>
@@ -29,10 +33,12 @@ const Cardslider = () => {
   };
 
   return (
+   <div className='w-[90%] flex flex-wrap pb-10 justify-around items-center mx-auto'>
+    <div className='lg:w-[40%] md:w-[50%] w-[100%] mt-10'>
     <div className="slider-container">
       <div className="slider">
         <div className="slider-image">
-          <img src={images[currentImageIndex]} alt={`Slide ${currentImageIndex + 1}`} />
+          <img className=' ' src={images[currentImageIndex]} alt={`Slide ${currentImageIndex + 1}`} />
         </div>
         <div className="thumbnails">
           {images.map((image, index) => (
@@ -41,7 +47,7 @@ const Cardslider = () => {
               className={`thumbnail ${index === currentImageIndex ? 'active' : ''}`}
               onClick={() => selectImage(index)}
             >
-              <img src={image} alt={`Thumbnail ${index + 1}`} />
+              <img className=''  src={image} alt={`Thumbnail ${index + 1}`} />
             </div>
           ))}
         </div>
@@ -52,6 +58,29 @@ const Cardslider = () => {
       <button className="next-btn" onClick={goToNext}>
         &gt;
       </button>
+    </div>
+    </div>
+    <div className='lg:w-[55%] md:w-[50%] w-[90%] mt-10'
+    >
+    {cardData ? (
+<div className=' '>
+    <h3 className='lg:text-[30px] md:text-[30px] text-[23px] font-bold'>{cardData.h1}</h3>
+    <p className='lg:text-[20px] md:text-[20px] text-[20px] font-bold'>Brand: {cardData.brand}</p>
+    <p className='lg:text-[20px] md:text-[20px] text-[20px]'>Price: <span style={{fontWeight:"800"}}> {cardData.price} </span> </p>
+    <p className='lg:text-[20px] md:text-[20px] text-[20px]'>Status: <span style={{fontWeight:"800"}}> {cardData.status} </span> </p>
+    <p className='lg:text-[20px] md:text-[20px] text-[20px]'> <span className='font-bold'> Description : </span><br/>  {cardData.detail}  </p>
+
+
+    {/* <p>Age: {cardData.age}</p> */}
+    {/* <p>Condition: {cardData.condition}</p> */}
+    {/* <p>Brand: {cardData.brand}</p> */}
+    {/* Add more fields if needed */}
+  </div>
+
+) : (
+  <p>No card data available.</p>
+)}
+</div> 
     </div>
   );
 };
--- src/data.js
+++ src/data.js
@@ -1,8 +1,12 @@
 export const cards=[
     {
-        img:"/image/laptop/lap.webp",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
+        img:[
+            "/image/laptop/lap.webp",
+            "/image/laptop/1.jpeg",
+            "/image/laptop/2.jpeg"
+        ],
+        status:"Out of Stock",
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
         h1:"MacBook Pro - Apple 2024",
         age:"Brand New",
         brand:"Apple",
@@ -10,19 +14,28 @@ export const cards=[
         price:"400,000"
     },
     {
-        img:"/image/laptop/10.jpeg",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
-        h1:"MacBook Pro - Apple 2023",
+        img:[
+            "/image/laptop/3.jpeg",
+            "/image/laptop/4.jpeg",
+        ],
+         status:"Avaliable",
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
+        h1:"i5 7 Generation - Dell 2023",
         age:"Brand New",
         brand:"Apple",
         condition:"Flawless",
         price:"350,000"
     },
     {
-        img:"/image/laptop/10.jpeg",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
+        img:[
+            "/image/laptop/5.jpeg",
+            "/image/laptop/6.jpeg",
+            "/image/laptop/7.jpeg"
+        ],
+
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
+
+         status:"Out of Stock",
         h1:"MacBook Pro - Apple 2024",
         age:"Brand New",
         brand:"Apple",
@@ -30,9 +43,14 @@ export const cards=[
         price:"400,000"
     },
     {
-        img:"/image/laptop/10.jpeg",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
+        img:[
+            "/image/laptop/9.jpeg",
+            "/image/laptop/10.jpeg",
+            "/image/laptop/11.jpeg"
+        ],
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
+
+         status:"Avaliable",
         h1:"MacBook Pro - Apple 2023",
         age:"Brand New",
         brand:"Apple",
@@ -40,9 +58,14 @@ export const cards=[
         price:"350,000"
     },
     {
-        img:"/image/laptop/10.jpeg",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
+        img:[
+            "/image/laptop/lap.webp",
+            "/image/laptop/11.jpeg",
+            "/image/laptop/12.jpeg"
+        ],
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
+
+         status:"Avaliable",
         h1:"MacBook Pro - Apple 2023",
         age:"Brand New",
         brand:"Apple",
@@ -50,9 +73,14 @@ export const cards=[
         price:"350,000"
     },
     {
-        img:"/image/laptop/10.jpeg",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
+        img:[
+            "/image/laptop/lap.webp",
+            "/image/laptop/11.jpeg",
+            "/image/laptop/12.jpeg"
+        ],
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
+
+         status:"Out of Stock",
         h1:"MacBook Pro - Apple 2024",
         age:"Brand New",
         brand:"Apple",
@@ -60,9 +88,14 @@ export const cards=[
         price:"400,000"
     },
     {
-        img:"/image/laptop/10.jpeg",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
+        img:[
+            "/image/laptop/lap.webp",
+            "/image/laptop/11.jpeg",
+            "/image/laptop/12.jpeg"
+        ],
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
+
+         status:"Avaliable",
         h1:"MacBook Pro - Apple 2023",
         age:"Brand New",
         brand:"Apple",
@@ -70,9 +103,14 @@ export const cards=[
         price:"350,000"
     },
     {
-        img:"/image/laptop/10.jpeg",
-        img1:"/image/laptop/11.jpeg",
-        img2:"/image/laptop/12.jpeg",
+        img:[
+            "/image/laptop/lap.webp",
+            "/image/laptop/11.jpeg",
+            "/image/laptop/12.jpeg"
+        ],
+        detail:"When it comes to laptops, the market offers a lot of choices, each catering to different needs and preferences. We take pride in presenting you with a vast range of laptops, carefully organized to align with the different demands of our customers. Whether you’re seeking a laptop for basic tasks like browsing and word processing, or you require a powerhouse for resource-intensive activities like gaming or video editing, we have you covered.  The laptop landscape is wide, and the options can sometimes be overwhelming. That’s where we step in to simplify your journey. Our user-friendly interface allows you to filter laptops based on your budget, preferred brands like Asus, Dell, and Lenovo, specifications, and more.  Laptops have revolutionized the way we live and work, offering a compact and portable computing solution that easily integrates into our daily lives. From powerful processors for smooth multitasking to stunning displays that bring your content to life, laptops encompass a wide range of features.",
+
+         status:"Avaliable",
         h1:"MacBook Pro - Apple 2023",
         age:"Brand New",
         brand:"Apple",
